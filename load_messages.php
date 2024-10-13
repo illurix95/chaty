9819dcd2c2dc407a788be0e6fb4690ee2f962dc0
Ryuga <130574835+Hackarofficial@users.noreply.github.com>
2024-10-13 21:13:49 +0530

    Update load_messages.php

--- load_messages.php
+++ load_messages.php
@@ -3,7 +3,7 @@ $file = 'messages.txt';
 if (file_exists($file)) {
     $messages = file($file, FILE_IGNORE_NEW_LINES);
     foreach ($messages as $msg) {
-        echo '<div>' . htmlspecialchars($msg) . '</div>';
+        echo '<div>' . htmlspecialchars($msg) . '</div><br>';
     }
 }
 ?>
